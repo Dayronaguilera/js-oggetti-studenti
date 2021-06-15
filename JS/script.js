499044a1c05c3fa6b8e7b886d85d5ecf3f843257
Dayronaguilera <aguilera.dayron@gmail.com>
2021-06-15 19:00:34 +0200

    refactoring

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,6 @@
 PARTE 1
 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. 
 Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
-
------------------------------------------------------------------------------------
-
-PARTE 2
-Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
-Ricordatevi, è un array, quindi? for.
-
--------------------------------------------------------------------------------------
-
-PARTE 3
-Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - 
-di aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
-Consigli:
-Le slide e il codice scritto oggi in classe vi saranno molto utili. Riguardatevi anche i video se possono essere utili. 
-Ragionate per step, un passo alla volta, e vedrete che alla fine il tutto risulterà molto semplice
 */
 
 var outputHtml = document.getElementById("output");
@@ -31,6 +16,11 @@ for (var key in student) {
     outputHtml.innerHTML += student[key] + ' ';
 }
 
+/*
+PARTE 2
+Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
+Ricordatevi, è un array, quindi? for.
+*/
 // seconda parte ---------------------------
 var output2 = document.getElementById("output2");
 
@@ -46,12 +36,19 @@ for (var i = 0; i < class1A.length; i++) {
 
     // per stampare tutte le proprietà key: value dell'oggetto
     for (var key in class1A[i]) {
-        output2.innerHTML += class1A[i][key] + '</br>';
+        output2.innerHTML += class1A[i][key] + ' ';
     }
     
 }
 
-
+/*
+PARTE 3
+Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - 
+di aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
+Consigli:
+Le slide e il codice scritto oggi in classe vi saranno molto utili. Riguardatevi anche i video se possono essere utili. 
+Ragionate per step, un passo alla volta, e vedrete che alla fine il tutto risulterà molto semplice
+*/
 // terza parte ------------------------------
 var output = document.getElementById("output3");
 
@@ -74,12 +71,8 @@ class1b.students.push({
 
 for (var i = 0; i < class1b.students.length; i++) {
 
-    // per stampare tutti i nomi dei passengers
-    // console.log(car.passengers[i].name)
-    
     // per stampare tutte le proprietà key: value dell'oggetto
     for (var key in class1b.students[i]) {
         output.innerHTML += class1b.students[i][key] + ' ';
-    }
-    
+    }   
 }
